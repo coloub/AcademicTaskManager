30fc4fd669d4072cfc9ce4c2a7d305e79a9e4cdc
coloub <jose1252jose@gmail.com>
2026-02-14 14:02:17 -0400

    #

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using AcademicTaskManager.Components;
 using AcademicTaskManager.Components.Account;
 using AcademicTaskManager.Data;
 using AcademicTaskManager.Services;
+using ASP.D_.CSE_325_MyProjects.Project.AcademicTaskManager.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
